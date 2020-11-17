e1e7e9db8aef33cb1a44d61b78e1719f424b5a02
fazaafrizal97 <72333197+fazaafrizal97@users.noreply.github.com>
2020-11-17 16:27:30 +0800

    rubah index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="refresh" content="0;url=dist/index.php">
     <title>Eartquake</title>
     <script language="javascript">
-        window.location.href = "dist/index.php"
+        window.location.href = "dist/indexhehee.php"
     </script>
 </head>
 
